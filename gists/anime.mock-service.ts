798f397cd926a0f647c9c8c68a915ad513170f4f
Dobroslav Totev <dev@totev.de>
2020-07-13 12:26:23 +0200

    chore: removed symlinks

--- gists/anime.mock-service.ts
+++ gists/anime.mock-service.ts
@@ -1,1 +1,22 @@
-/Users/totev/dev/private/ts-mocked-deps/vue.js/mock/services/remote-api/anime.mock-service.ts
+import faker from "faker";
+import { Anime } from "../../../src/models/anime";
+import { RemoteAnimeService } from "../../../src/services/remote-api/remote.service";
+
+function createAnimeMock(): Anime {
+  return {
+    id: faker.random.uuid(),
+    attributes: {/*... */},
+  };
+}
+
+export class MockAnimeService implements RemoteAnimeService {
+  async fetchTrending(): Promise<any> {
+    const result = createAnimeMock();
+    return new Promise((resolve) => {
+      // why not add a delay to make it more realistic
+      setTimeout(() => {
+        resolve(result);
+      }, faker.random.number(1500));
+    });
+  }
+}
--- gists/jest.config.js
+++ gists/jest.config.js
@@ -1,1 +1,8 @@
-/Users/totev/dev/private/ts-mocked-deps/vue.js/jest.config.js
+const deepmerge = require("deepmerge");
+const defaultPreset = require("@vue/cli-plugin-unit-jest/presets/typescript/jest-preset");
+
+module.exports = deepmerge(defaultPreset, {
+  moduleNameMapper: {
+    "^@remote-api/(.*)$": "<rootDir>/mock/services/remote-api/$1"
+  }
+});
